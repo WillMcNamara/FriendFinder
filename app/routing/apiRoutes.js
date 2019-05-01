0e9807e2050c8d0299f1bc05142a72b4fd7085dc
Will M <wmcna97@gmail.com>
2019-05-01 14:39:50 -0400

    fixed files, added .env, some basic code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,7 @@
+app.get("/api/friends", function(res, req){
+    res.json(friends)
+})
+
+app.post("/api/friends", function(res, req){
+    
+})
---
+++ server.js
@@ -0,0 +1,16 @@
+// Dependencies
+require('dotenv').config();
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+let PORT = process.env.PORT;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
